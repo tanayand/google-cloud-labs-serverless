3bcc2373228ff9e646e2a76b42fce34101143ed0
Tetiana Sobko <47154900+tanayand@users.noreply.github.com>
2021-03-25 01:41:44 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -9,47 +9,47 @@ exports.compute = (req, res) => {
   let num = req.body.compute;
   let result;
 
-    function fiboArray(num) {
-      let index = 0;
-      let fibNumbers = [];
-      let memo = {};
-      let fibNum;
+  function fiboArray(num) {
+    let index = 0;
+    let fibNumbers = [];
+    let memo = {};
+    let fibNum;
 
-      function fib(n) {
-        if (n in memo) { 
-          return memo[n] 
-        } else { 
-            if (n <= 1) { 
-              memo[n] = n 
-            } else { 
-              memo[n] = fib(n - 1) + fib(n - 2) 
-            } 
-            return memo[n]
-        }
+    function fib(n) {
+      if (n in memo) { 
+        return memo[n] 
+      } else { 
+          if (n <= 1) { 
+            memo[n] = n 
+          } else { 
+            memo[n] = fib(n - 1) + fib(n - 2) 
+          } 
+          return memo[n]
       }
-
-      do {
-        fibNum = fib(index);
-        fibNumbers.push(fibNum);
-        index++;
-      } while (fibNum < num) 
-
-      return fibNumbers.slice(0, fibNumbers.length - 1);
     }
 
-    function isOdd(num) { 
-      return num % 2;
-    }
+    do {
+      fibNum = fib(index);
+      fibNumbers.push(fibNum);
+      index++;
+    } while (fibNum <= num) 
 
-    function sumOdd(numArray) {
-      let sum = 0;
-      for (var i = 0; i < numArray.length; i++) {		
-        if ( isOdd( numArray[i] ) ) {
-          sum += numArray[i];
-        }				
-      }
-      return sum;
+    return fibNumbers.slice(0, fibNumbers.length - 1);
+  }
+
+  function isOdd(num) { 
+    return num % 2;
+  }
+
+  function sumOdd(numArray) {
+    let sum = 0;
+    for (var i = 0; i < numArray.length; i++) {		
+      if ( isOdd( numArray[i] ) ) {
+        sum += numArray[i];
+      }				
     }
+    return sum;
+  }
 
     
   if (Number.isInteger(num)) {
